9b528317c1828649d15257d9651ea4d471437920
aTool <i@atool.org>
2016-01-02 11:48:22 +0800

    update test

--- test/test_memory_cache.py
+++ test/test_memory_cache.py
@@ -17,9 +17,8 @@ def need_cache_function(input):
     return random.randint(1, 100)
 
 if __name__ == "__main__":
-	rst = ''
+	print('##start test memory cache...')
 	for i in range(10):
 		sleep(1)
-		rst = rst + ', ' + str(need_cache_function(2))
-	rst = rst[2:]
-	print(rst)
+		print(need_cache_function(2))
+	print('##end test memory cache...')
