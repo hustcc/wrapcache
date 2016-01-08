577dfc052f50d3f7b23a47ccf032066889ec79a1
hustcc <i@hust.cc>
2016-01-08 11:57:41 +0800

    update redis testcase

--- wrapcache/adapter/RedisAdapter.py
+++ wrapcache/adapter/RedisAdapter.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
 			
 			#test set / get
 			self.test_class.set(key, value)
-			self.assertEqual(self.test_class.get(key), value)
+			self.assertEqual(self.test_class.get(key).decode('utf-8'), value)
 			time.sleep(4)
 			self.assertRaises(CacheTimeoutException, self.test_class.get, key)
 			
