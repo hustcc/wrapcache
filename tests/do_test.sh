5f1ed3210863f621d091b19ed08d9cc0e75e74f8
aTool <i@atool.org>
2016-01-12 00:10:40 +0800

    update testcase

--- tests/do_test.sh
+++ tests/do_test.sh
@@ -2,4 +2,4 @@
 coverage run wrapcache/database/__init__.py
 coverage run tests/test_unitest.py
 coverage run wrapcache/adapter/MemoryAdapter.py
-coverage run wrapcache/adapter/RedisAdapter.py
+# coverage run wrapcache/adapter/RedisAdapter.py
