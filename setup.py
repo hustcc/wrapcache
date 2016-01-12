f524d0ff7b9f5faa0b6922c2a7acfc19775b939b
hustcc <i@hust.cc>
2016-01-12 09:31:15 +0800

    v1.0.5

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ GitHub: https://github.com/hustcc/wrapcache
 """
 
 setup(name = 'wrapcache',
-      version = '1.0.4',
+      version = '1.0.5',
       description = 'Short for wraps cache. A method cache system base on method Decorators.',
       long_description = LONGDOC,
       author = 'hustcc',
